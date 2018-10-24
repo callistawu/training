a149ff3a700758f50b7b298ca8811382a99fb5df
Callista Wu <callistawu@gmail.com>
2018-10-23 22:17:35 -0700

    Add files via upload

---
+++ ts-react-gatsby/db-ts-react-gatsby/index.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+    return a + b;
+}
+alert(add(4, 5));
+alert(add(4, 'five'));
---
+++ ts-react-gatsby/db-ts-react-gatsby/index.ts
@@ -0,0 +1,12 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+alert(add(4, 5));
+// alert(add(4, 'five'));
+
+enum Suit {
+  HEARTS,
+  CLUBS,
+  SPADES,
+}
